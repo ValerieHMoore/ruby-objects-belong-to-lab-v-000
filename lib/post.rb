95ab6f918425dd39e456c28f7c0d181e9fba477e
Valerie Moore <periwinkle17@outlook.com>
2018-06-30 00:39:08 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Posts
+  attr_accessor :title, :author
+ 
+  def initialize(title)
+    @title = title
+    @author = authort
+  end
+end
+
+class Author
+ attr_accessor :name
+ 
+  def initialize
+    @name = name
+  end
+  
+end
