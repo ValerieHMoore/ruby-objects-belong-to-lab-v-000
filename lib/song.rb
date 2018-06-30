1f5f341bb5e5eefeb4fd5ce6090ad71e78e0ab20
Valerie Moore <periwinkle17@outlook.com>
2018-06-30 00:38:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,17 @@
 class Song
- 
   attr_accessor :title, :artist
  
   def initialize(title)
     @title = title
+    @artist = artist
   end
+end
+
+class Artist
+ attr_accessor :name
  
+  def initialize
+    @name = name
+  end
+  
 end
